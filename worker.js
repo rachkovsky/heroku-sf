39d5d7a246aeda34aa93a4b2415e94e1d5894245
Pavel Rachkovsky <pavelrachkovsky@Pavels-MacBook-Pro.local>
2020-10-14 15:35:25 +0300

    fix

--- worker.js
+++ worker.js
@@ -8,28 +8,20 @@ let workers = process.env.WEB_CONCURRENCY || 2;
 let maxJobsPerWorker = 5;
 
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 function start() {
     // Connect to the named work queue
     let workQueue = new Queue('work', REDIS_URL);
 
     workQueue.process(maxJobsPerWorker, function (job, done) {
-
-        console.log('------- ', job)
-        client.query('INSERT INTO cases (case_id, status) VALUES ($1, $2)', ['123123123', 'new'], (err, result) => {
+        console.log('job ------- ', job.data)
+        client.query('INSERT INTO cases (case_id, status) VALUES ($1, $2)', [job.data.Id, job.data.Status], (err, result) => {
             if (err) {
                 console.log(err);
                 done();
             }
             done();
-            console.log(result);
         });
-        return { value: "This will be stored" };
     });
 }
-
 
 throng({ workers, start });
