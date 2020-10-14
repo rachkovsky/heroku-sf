be59f5631be491dc9d9235cf630af90e9fb66331
Pavel Rachkovsky <pavelrachkovsky@Pavels-MacBook-Pro.local>
2020-10-15 00:33:01 +0300

    fix

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 const { Client } = require('pg');
 
 const connectionString = process.env.DATABASE_URL;
-const client = new Client(connectionString);
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+});
 
 module.exports = client;
